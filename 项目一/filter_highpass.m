3659dc1964ef609b5ea8fccae44c046b27b85885
TKOSophia <38215310+TKOSophia@users.noreply.github.com>
2019-10-24 00:40:04 +0800

    滑动条和缩放部分的完善+坐标修改
    滑动条和缩放部分可以正常使用，但目前只使用导入信号进行了调试，数据点数小于100时滑动条隐去，缩放条只控制channel 0，电压轴缩放是四条通道同时控制的。导入信号后，移动滑动条，信号放大到画面110个点（可以根据意见再修改）。

---
+++ 项目一/filter_highpass.m
@@ -0,0 +1,14 @@
+function[n,y]=filter_highpass(Fs,x,fc)
+% % 示例信号用于检测滤波器
+% Fs=200;N=256;fc=50;
+% n=0:N-1;
+% t=n/Fs;
+% x=sign(sin(2*pi*15*t));   
+N=length(x);
+n=0:N-1;
+Wc=2*fc/Fs;                                          
+[b,a]=butter(4,Wc,'high');
+y = filter(b,a,x);
+% figure(1);subplot(2,1,1);plot(n,x);
+% % 显示格式
+% figure(1);subplot(2,1,2);plot(n,y);  
