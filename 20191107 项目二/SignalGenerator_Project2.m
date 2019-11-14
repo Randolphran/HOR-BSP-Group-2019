3a70e5121bcc4477d91ef90712664f24ee06e0c8
Randolphran <55443676+Randolphran@users.noreply.github.com>
2019-11-14 16:39:56 +0800

    Debug：edit_dutycycle重定义
    删除了该box的重复回调和构造函数

--- 20191107 项目二/SignalGenerator_Project2.m
+++ 20191107 项目二/SignalGenerator_Project2.m
@@ -411,26 +411,3 @@ function edit_dutycycle_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-
-
-function edit_dutycycle_Callback(hObject, eventdata, handles)
-% hObject    handle to edit_dutycycle (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit_dutycycle as text
-%        str2double(get(hObject,'String')) returns contents of edit_dutycycle as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit_dutycycle_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit_dutycycle (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
