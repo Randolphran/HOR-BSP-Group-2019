ce2a3a5329b01732655cfd4827e45fe465dc68e8
TKOSophia <38215310+TKOSophia@users.noreply.github.com>
2019-11-21 14:47:12 +0800

    输出模块的三个按键的回调函数+staticAO

--- 20191107 项目二/SignalGenerator_Project2.m
+++ 20191107 项目二/SignalGenerator_Project2.m
@@ -22,7 +22,7 @@ function varargout = SignalGenerator_Project2(varargin)
 
 % Edit the above text to modify the response to help SignalGenerator_Project2
 
-% Last Modified by GUIDE v2.5 21-Nov-2019 13:25:08
+% Last Modified by GUIDE v2.5 21-Nov-2019 14:05:07
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -73,6 +73,18 @@ handles.wavechosen=wavechosen;
 dataAO=zeros(512,1);
 handles.dataAO=dataAO;
 guidata(hObject,handles);
+
+contiflag=1;
+handles.contiflag=contiflag;
+
+periodNum=2;
+handles.periodNum=periodNum;
+
+i=0;
+handles.i=i;
+
+periodcount=0;
+handles.periodcount=periodcount;
 
 set(handles.listbox1,'value',1);
 
@@ -366,6 +378,7 @@ function pushbutton_run_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_run (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+StaticAO(handles.figure1,handles);
 
 
 % --- Executes on button press in pushbutton_pause.
@@ -373,13 +386,19 @@ function pushbutton_pause_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_pause (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+global t;
+stop(t);
 
 % --- Executes on button press in pushbutton_stop.
 function pushbutton_stop_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_stop (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global t;
+stop(t);
+delete(t)
+clear global t;
+
 uiresume(handles.figure1);
 
 
@@ -475,3 +494,12 @@ handles.contiflag=contiflag;
 guidata(hObject,handles);
 % Hint: get(hObject,'Value') returns toggle state of radiobutton_continuous
 
+
+
+% --- Executes on button press in radiobutton_continuous.
+function radiobutton_continuous_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton_continuous (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton_continuous
