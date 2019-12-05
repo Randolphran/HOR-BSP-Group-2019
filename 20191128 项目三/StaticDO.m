a4175c4e11313728817f3498970409c57ec47141
TKOSophia <38215310+TKOSophia@users.noreply.github.com>
2019-12-05 16:25:12 +0800

    修改edit-time，timer加上hObject
    1. edit框改成了读取string，而非value
    2. timer的计数从persistent i改为handles内保存

--- 20191128 项目三/StaticDO.m
+++ 20191128 项目三/StaticDO.m
@@ -28,6 +28,7 @@ deviceDescription = 'USB-4704,BID#0';
 % startPort = int32(0);
 % portCount = int32(1);
 global t;
+
 contiflag=handles.contiflag;
 period=handles.period;
 period=period/2;
@@ -51,7 +52,7 @@ try
     % Step 3: Write DO ports
     bufferForWriting = NET.createArray('System.Byte', int32(64));
     t = timer('TimerFcn', {@TimerCallback, instantDoCtrl, ...
-        bufferForWriting, N, contiflag},'period', period, ...
+        bufferForWriting, N, contiflag, hObject},'period', period, ...
         'executionmode', 'fixedrate', 'StartDelay', 1);
     start(t);
     uiwait(handles.figure1);
@@ -111,12 +112,9 @@ result =  errorCode < Automation.BDaq.ErrorCode.Success && ...
 end
 
 function TimerCallback(obj, event, instantDoCtrl, bufferForWriting, N,...
-    contiflag)
-
-persistent i;
-if isempty(i)
-    i=0;
-end
+    contiflag,hObject)
+handles = guidata(hObject);
+i=handles.i;
 i=i+1;
 if mod(i,2)==0
     strData = 0;   
@@ -132,13 +130,14 @@ end
     
 if contiflag==0
    if i==N
-       clear i;
+       i=0;
        stop(obj);
        delete(obj);
        uiresume(handles.figure1);
    end
 end
-
+handles.i=i;
+guidata(hObject,handles);
 end
 
 
